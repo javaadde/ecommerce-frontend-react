15a56cc79807dc0aac5badacdcb5cbd79bfb6e34
javad-kuttappi <javaadde123@hotmail.com>
2025-09-20 16:57:27 +0530

    created a vite lastest and inetialized with folders and files and added theme color and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+ module.exports = {
+      theme: {
+        colors: {
+          'primary-white': '#eee',
+          'secondary-white': '#fff',
+          'primary-dark':'#000'
+        },
+      },
+      plugins: [],
+};
